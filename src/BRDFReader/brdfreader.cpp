8f86baf4fbb6538df2702dda1bd1ed11a97c87bd
karim <karimsalama94@hotmail.com>
2018-11-16 16:43:42 +0100

    Computation of covariance matrix of 5 BRDFs (test)

--- src/BRDFReader/brdfreader.cpp
+++ src/BRDFReader/brdfreader.cpp
@@ -187,54 +187,46 @@ void lookup_brdf_val(double* brdf, double theta_in, double fi_in,
 
 
 // Read BRDF data
-bool BRDFReader::read_brdf(const char *filename, double* &brdf)
-{
-        FILE *f = fopen(filename, "rb");
-        if (!f)
-                return false;
+bool BRDFReader::read_brdf(const char *filename, double* &brdf){
+	FILE *f = fopen(filename, "rb");
+	if (!f)return false;
 
-        int dims[3];
-        fread(dims, sizeof(int), 3, f);
-        int n = dims[0] * dims[1] * dims[2];
-        std::cout << "n read : " << n << std::endl;
-        if (n != BRDF_SAMPLING_RES_THETA_H *
-                 BRDF_SAMPLING_RES_THETA_D *
-                 BRDF_SAMPLING_RES_PHI_D / 2)
-        {
-                std::cerr << "Dimensions don't match" << std::endl ;
-                fclose(f);
-                return false;
-        }
+	int dims[3];
+	fread(dims, sizeof(int), 3, f);
+	int n = dims[0] * dims[1] * dims[2];
+	std::cout << "n read : " << n << std::endl;
+	if (n != BRDF_SAMPLING_RES_THETA_H *
+			 BRDF_SAMPLING_RES_THETA_D *
+			 BRDF_SAMPLING_RES_PHI_D / 2){
+			std::cerr << "Dimensions don't match" << std::endl ;
+			fclose(f);
+			return false;
+	}
 
-        brdf = (double*) malloc (sizeof(double)*3*n);
-        fread(brdf, sizeof(double), 3*n, f);
+	brdf = (double*) malloc (sizeof(double)*3*n);
+	fread(brdf, sizeof(double), 3*n, f);
 
-        fclose(f);
+	fclose(f);
 
-        //TODO : split ici pour créééééer une matrice à partir de brdf
+	//TODO : split ici pour créééééer une matrice à partir de brdf
 
-        n = 16;
-        for (int i = 0; i < n; i++)
-        {
-            double theta_in = i * 0.5 * M_PI / n;
-            for (int j = 0; j < 4*n; j++)
-                {
-                        double phi_in = j * 2.0 * M_PI / (4*n);
-                        for (int k = 0; k < n; k++)
-                        {
-                                double theta_out = k * 0.5 * M_PI / n;
-                                for (int l = 0; l < 4*n; l++)
-                                {
-                                        double phi_out = l * 2.0 * M_PI / (4*n);
-                                        double red,green,blue;
-                                        lookup_brdf_val(brdf, theta_in, phi_in, theta_out, phi_out, red, green, blue);
-                                        if ( i+j+k+l == 0) {
-                                            printf("%f %f %f\n", (float)red, (float)green, (float)blue);
-                                        }
-                                }
-                        }
-            }
-        }
+	n = 16;
+	for (int i = 0; i < n; i++){
+		double theta_in = i * 0.5 * M_PI / n;
+		for (int j = 0; j < 4*n; j++){
+			double phi_in = j * 2.0 * M_PI / (4*n);
+			for (int k = 0; k < n; k++){
+				double theta_out = k * 0.5 * M_PI / n;
+				for (int l = 0; l < 4*n; l++){
+					double phi_out = l * 2.0 * M_PI / (4*n);
+					double red,green,blue;
+					lookup_brdf_val(brdf, theta_in, phi_in, theta_out, phi_out, red, green, blue);
+					if ( i+j+k+l == 0)
+						printf("%f %f %f\n", (float)red, (float)green, (float)blue);
+				}
+			}
+		}
+	}
 
-        return true;
+	return true;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,42 @@
 #include <iostream>
 #include <Eigen/Dense>
+
+#include <Eigen/Core>
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include "BRDFReader/brdfreader.h"
+#include <vector>
+#include <string>
+#include <iostream>
 
-using Eigen::MatrixXd;
+#include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    /*MatrixXd m(2,2);
-    m(0,0) = 3;
-    m(1,0) = 2.5;
-    m(0,1) = -1;
-    m(1,1) = m(1,0) + m(0,1);
-    std::cout << m << std::endl;*/
-    BRDFReader reader;
-    double* brdf;
-    reader.read_brdf("../datas/silver-metallic-paint2.binary", brdf);
+using namespace Eigen;
 
+int main(int, char *[]) {
+	std::vector<std::string> files={
+		"../data/silver-metallic-paint2.binary",
+		"../data/cherry-235.binary",
+		"../data/chrome.binary",
+		"../data/natural-209.binary",
+		"../data/pink-fabric.binary"
+	};
 
-    return 0;
+	const uint BRDFsize = 1458000*3; //16*16*64*64*3;
+
+	MatrixXd merleData(BRDFsize,files.size());
+
+	BRDFReader reader;
+	for(uint i=0; i<files.size();i++){
+		double *brdf = (double*)malloc(BRDFsize*sizeof(double));
+		reader.read_brdf(files[i].c_str(), brdf);
+		Map<VectorXd> vec(brdf,BRDFsize);
+		merleData.col(i)=vec- Map<VectorXd>::Ones(BRDFsize)*vec.mean();
+		free(brdf);
+	}
+//	std::cout << merleData<< std::endl;
+	std::cout << merleData.transpose()*merleData << std::endl;
+	return 0;
 }
 
 

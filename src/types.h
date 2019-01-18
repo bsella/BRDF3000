c263f1d35579d3757a6d8cb5571c12f8a660d17c
Adrien Grosjean <grosjeanadrien@gmail.com>
2019-01-18 13:09:07 +0100

    Creation of namespace ChefDevr Creation of type.h that contains template types for the matrices and vectors Correction of PCASolver.h -> static functions, types, and comments

---
+++ src/types.h
@@ -0,0 +1,18 @@
+#ifndef TYPES__H
+#define TYPES__H
+
+#include <Eigen/Core>
+
+namespace ChefDevr
+{
+    template<typename Scalar>
+    using Matrix = Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic>;
+    
+    template<typename Scalar>
+    using Vector = Eigen::Matrix<Scalar, Eigen::Dynamic, 1>;
+    
+    template<typename Scalar>
+    using RowVector = Eigen::Matrix<Scalar, 1, Eigen::Dynamic>;
+} // namespace ChefDevr
+
+#endif //TYPES__H

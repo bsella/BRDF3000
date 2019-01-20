ca7ab97896084974ff93ec1abb3f7307873af3a8
Adrien Grosjean <grosjeanadrien@gmail.com>
2019-01-20 11:05:51 +0100

    Implementation of the learning displacement TODO : implement covariance vector computation + inverse and determinant update with Sherman-Morisson

--- src/Optimisation/OptimisationSolver.h
+++ src/Optimisation/OptimisationSolver.h
@@ -2,6 +2,7 @@
 #define OPTIMISATIONSOLVER_H
 
 #include "../types.h"
+#include <cmath>
 
 namespace ChefDevr
 {
@@ -10,9 +11,16 @@ namespace ChefDevr
     public:
         struct OptiResult
         {
+            OptiResult(const OptiResult& other):
+                K(other.K),
+                K_minus1(other.K_minus1),
+                X(other.X),
+                cost(other.cost){}
+                
             Matrix<Scalar> K; // Mapping matrix
             Matrix<Scalar> K_minus1; // Inverse mapping matrix
             Vector<Scalar> X; // Latent variables vector
+            Scalar cost; // Value of the cost function for this solution
         };
         
         OptimisationSolver() = delete;
@@ -21,8 +29,8 @@ namespace ChefDevr
         /**
         * @brief  Compute an optimised mapping from BRDFs space to a latent space
         * whose dimension is specified by dim
-        * @param  Z The BRDFs data matrix
-        * @param  dim The dimension of latent space
+        * @param  Z BRDFs data matrix
+        * @param  dim Dimension of latent space
         * @param  minStep Step beyond which convergence is considered being reached
         * @return Structure containing optimized mapping, inverse mapping and latent variables
         * 
@@ -34,35 +42,100 @@ namespace ChefDevr
             float minStep);
         
     private:
-        static constexpr float reduceStep = .5f;
-
+        static constexpr Scalar reduceStep = .5f;
+        static constexpr Scalar mu = 0.0001f;
+        static constexpr Scalar l = 1.0f;
+        
         /**
         * @brief Computes the cost of the inverse mapping defined by K_minus1
         * @param detK Determinant of the forward mapping matrix K
         * @param K_minus1 Inverse mapping matrix
         * @param ZZt Z*Ztransposed
-        * @param d The number of BRDFs in Z (Z.cols())
+        * @param nb_data The number of BRDFs in Z (Z.cols())
         */
         static Scalar cost(
             const Scalar& detK,
             const Matrix<Scalar>& K_minus1,
             const Matrix<Scalar>& ZZt,
-            const unsigned int d);
+            const unsigned int nb_data);
 
         /**
-        * @brief  Modify the X vector to find a better solution
-        * @return New X vector, with lower cost function
+        * @brief Finds a displacement vector of X that improves the solution
+        * @return Displacement vector
+        * @param X Latent variables vector
+        * @param K_minus1 Inverse mapping matrix
+        * @param detK Determinant of the mapping matrix K
+        * @param cost Value of the cost function for this solution
+        * @param ZZt Z*Ztransposed
+        * @param step Step
+        * @param dim Latent space dimension
+        * @return A displacement vector of X that improves the solution
         * 
         * Adds and substracts _reduceStep_ from each element
         * and reevaluate the cost function to check for better solutions
         */
-        static OptiResult computeExplorationDisplacement (const OptiResult& optiRes);
+        static Vector<Scalar> computeExplorationDisplacement (
+            Vector<Scalar>& X,
+            Matrix<Scalar>& K_minus1
+            Scalar& detK,
+            Scalar& cost,
+            const Matrix<Scalar>& ZZt,
+            const Scalar step,
+            const unsigned char dim,
+            const unsigned int nb_data);
 
         /**
-        * @brief Apply a previously calculated displacement to each element of X
+        * @brief Apply a previously computed displacement to each element of X
         * @return New modified vector
         */
         static OptiResult computeLearnDisplacement (const OptiResult& optiRes);
+        
+        /**
+         * @brief Updates the inverse matrix K_minus1 with Sherman-Morisson formula
+         * @param K_minus1 Inverse mapping matrix
+         * @param lv_num Number of the latent variable that has changed
+         * @param cov_vector The column of K that has changed
+         */
+        static Matrix<Scalar> updateInverse (
+            const Matrix<Scalar>& K_minus1,
+            unsigned int lv_num,
+            Vector<Scalar>& cov_vector);
+        
+        /**
+         * @brief Updates the determinant of the matrix K with Sherman-Morisson formula
+         * @param K_minus1 Inverse mapping matrix
+         * @param lv_num Number of the latent variable that has changed
+         * @param cov_vector The column of K that has changed
+         */
+        static Matrix<Scalar> updateDeterminant (
+            const Matrix<Scalar>& K_minus1,
+            unsigned int lv_num,
+            Vector<Scalar>& cov_vector);
+        
+        /**
+         * @brief Computes the covariance vector (usually stored in K)
+         * of the lv_num'th latent variable
+         * @param X Latent variables vector
+         * @param lv_num Number of the latent variable for the cov vector to be computed
+         */
+        static Vector<Scalar> computeCovVector (
+            const Vector<Scalar>& X,
+            unsigned int lv_num);
+        
+        /**
+         * @brief Covariance function given in the research paper :
+         * A Versatile Parametrization for Measured Materials Manifold
+         * @param x1 first latent variable
+         * @param x2 second latent variable
+         * @return Covariance value
+         */
+        static inline Scalar covariance(Vector<Scalar>& x1, Vector<Scalar>& x2)
+        {
+            const Scalar norm_x1_x2((x1-x2).norm());
+            const Scalar exp_part(std::exp(-norm_x1_x2*norm_x1_x2/(Scalar(2)*l*l)));
+            // dirac(x1-x2) <=> norm(x1-x2) == 0
+            return norm_x1_2 < 0.00001 ? mu + exp_part : exp_part;
+        }
     };
 } // namespace ChefDevr
 
--- src/Optimisation/OptimisationSolver.hpp
+++ src/Optimisation/OptimisationSolver.hpp
@@ -17,25 +17,97 @@ namespace ChefDevr
         const Scalar& detK,
         const Matrix<Scalar>& K_minus1,
         const Matrix<Scalar>& ZZt,
-        const unsigned int d
+        const unsigned int nb_data
                                            )
     {
-        Scalar trace(0.0);
-        
+        Scalar trace(0);
         // Compute trace of K_minus1 * ZZt
         # pragma omp parallel for reduction(+:trace)
         for (unsigned int i(0); i < ZZt.cols(); ++i){
             trace += K_minus1.row(i).dot(ZZt.col(i));
         }
-        
-        return Scalar(0.5) * d * std::log(detK) + Scalar(0.5) * trace;
+        return Scalar(0.5) * nb_data * std::log(detK) + Scalar(0.5) * trace;
     }
     
     template <typename Scalar>
-    typename OptimisationSolver<Scalar>::OptiResult OptimisationSolver<Scalar>::computeExplorationDisplacement (
-        const OptiResult& optiRes)
+    typename Vector<Scalar> OptimisationSolver<Scalar>::computeExplorationDisplacement (
+        Vector<Scalar>& X,
+        Matrix<Scalar>& K_minus1
+        Scalar& detK,
+        Scalar& cost,
+        const Matrix<Scalar>& ZZt,
+        const Scalar step,
+        const unsigned char dim,
+        const unsigned int nb_data)
     {
-        return OptiResult();
+        const auto& nbcoefs(optiRes.X.rows());
+        Scalar new_cost;
+        Vector<Scalar> new_X(X);
+        Vector<Scalar> cov_vector(X.rows()*0.5), new_cov_vector(X.rows()*0.5);
+        Matrix<Scalar> new_K_minus1(K_minus1.rows(), K_minus1.cols());
+        Vector<Scalar> X_move(nbcoefs);
+        unsigned int lv_num;
+        
+        for (unsigned int i(0); i < nbcoefs;++i)
+        {
+            lv_num = i/dim;
+            X_move[i] += step;
+            new_X[i] += step;
+            new_cov_vector = computeCovarianceVector(newX, lv_num)
+            
+            // Update K_minus1 and detK with Sherman-Morisson formula
+            new_K_minus1 = updateInverse(K_minus1, lv_num, new_cov_vector);
+            new_detK = updateDeterminant(new_K_minus1, lv_num, new_cov_vector);
+            // Update cost
+            new_cost = cost(new_detK, new_K_minus1, ZZt, nb_data);
+            
+            if (new_cost > cost){
+                X_move[i] -= Scalar(2)*step;
+                new_X[i] -= Scalar(2)*step;
+                new_cov_vector = computeCovVector(newX, lv_num)
+                
+                // Update K_minus1 and detK with Sherman-Morisson formula
+                new_K_minus1 = updateInverse(K_minus1, lv_num, new_cov_vector);
+                new_detK = updateDeterminant(new_K_minus1, lv_num, new_cov_vector);
+                // Update cost
+                new_cost = cost(new_detK, new_K_minus1, ZZt, nb_data);
+                
+                if (new_cost > cost){
+                    X_move[i] += step;
+                    new_X[i] += step;
+                }
+                else{
+                    cost = new_cost;
+                    K_minus1 = new_K_minus1;
+                    detK = new_detK;
+                }
+            }
+            else{
+                cost = new_cost;
+                K_minus1 = new_K_minus1;
+                detK = new_detK;
+            }
+        }
+        X = X+X_move;
+        return X_move;
+    }
+    
+    template <typename Scalar>
+    typename Matrix<Scalar> updateInverse (
+        const Matrix<Scalar>& K_minus1,
+        unsigned int lv_num,
+        Vector<Scalar>& cov_vector)
+    {
+        return Matrix<Scalar>();
+    }
+    
+    template <typename Scalar>
+    typename Matrix<Scalar> updateDeterminant (
+        const Matrix<Scalar>& K_minus1,
+        unsigned int lv_num,
+        Vector<Scalar>& cov_vector)
+    {
+        return Matrix<Scalar>();
     }
     
     template <typename Scalar>
@@ -44,4 +116,5 @@ namespace ChefDevr
     {
         return OptiResult();
     }
+    
 } // namespace ChefDevr

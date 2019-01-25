4b7c6702b7ac67c181979688aaa69b4aab6ac657
burg ludovic <brl1547a@u3-112-06l.ens.fsi>
2019-01-25 17:53:25 +0100

    Test application's working

--- src/Optimisation/OptiDataWriter.hpp
+++ src/Optimisation/OptiDataWriter.hpp
@@ -9,10 +9,10 @@ namespace ChefDevr
 {
     void writeAlbedoMap (const std::string& path, const unsigned width = 1024, const unsigned height = 1024)
     {
-        Color color;
+        /*Color color;
         Vector<Scalar> brdf;
         for (;;) {
             color = computeAlbedo(brdf);
-        }
+        }*/
     }
 } // namespace ChevDevr  

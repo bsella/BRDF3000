ebac5f9485b43fa297fc92c2ee706b84f84ed48c
Adrien Grosjean <grosjeanadrien@gmail.com>
2019-02-02 14:11:09 +0100

    Test the output of a reconstructed BRDF for comparison -> fails : negative values

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,22 @@
 using namespace ChefDevr;
 using Scalar = double;
 
+void writeBRDF(const std::string& path, const Vector<Scalar>& brdf)
+{
+    std::ofstream file(path);
+    if (!file.is_open()){
+        std::cerr << "Could not create file \"" << path<< "\"" << std::endl;
+    }
+    file << brdf;
+}
+
 int main(int numArguments, const char *argv[]) {
+    std::cout << std::endl;
     // Récupérer les arguments
     // Afficher le message d'aide si faut
     
     std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
     std::chrono::duration<double, std::micro> duration;
-        
     BRDFReader reader;
     const unsigned int dim = 2;
     const Scalar minStep = 0.5;
@@ -26,31 +35,40 @@ int main(int numArguments, const char *argv[]) {
     auto Z = reader.createZ<Scalar>(brdfsDir);
     RowVector<Scalar> meanBRDF(Z.cols());
     centerMat(Z, meanBRDF);
+    Vector<Scalar> brdf0_r(Z.cols());
     
     
-    OptimisationSolver<Scalar> optimisation{minStep, Z, dim};
+    OptimisationSolver<Scalar> optimizer{minStep, Z, dim};
     
     start = std::chrono::system_clock::now();
-    optimisation.optimizeMapping();
+    optimizer.optimizeMapping();
     end = std::chrono::system_clock::now();
     duration = end - start;
-    std::cout << "Optimisation took " << duration.count() << " micro seconds" << std::endl;
+    std::cout << "Optimisation took " << duration.count() << " micro seconds" << std::endl << std::endl;
     
     BRDFReconstructor<Scalar> reconstructor(
                 Z,
-                optimisation.getInverseMapping(),
-                optimisation.getLatentVariables(),
+                optimizer.getInverseMapping(),
+                optimizer.getLatentVariables(),
                 meanBRDF.transpose(),
                 dim);
     
-    std::cout << "Reconstruction error for BRDF n°0 : " << reconstructor.reconstructionError(0) << std::endl;
+    start = std::chrono::system_clock::now();
+    reconstructor.reconstruct(brdf0_r, optimizer.getLatentVariables().segment(0,dim));
+    end = std::chrono::system_clock::now();
+    duration = end - start;
+    std::cout << "Reconstruction took " << duration.count()*0.000001 << " seconds" << std::endl << std::endl;
+    writeBRDF("../brdf0.binary", brdf0_r);
+    
+    
+    std::cout << "Reconstruction error for BRDF n°0 : " << reconstructor.reconstructionError(0) << std::endl << std::endl;
     
     /*
     start = std::chrono::system_clock::now();
     writeAlbedoMap<Scalar>(mapPath,
                    Z,
-                   optimisation.getLatentVariables(),
-                   optimisation.getInverseMapping(),
+                   optimizer.getLatentVariables(),
+                   optimizer.getInverseMapping(),
                    meanBRDF,
                    dim,
                    albedoSampling,

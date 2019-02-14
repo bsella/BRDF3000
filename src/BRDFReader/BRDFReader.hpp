85fd1c5bbc733f8c34a6a860a2cc27c2e2620fde
slowlys <adele.sd@online.fr>
2019-02-14 18:56:32 +0100

    BRDF Reader in parallel

--- src/BRDFReader/BRDFReader.hpp
+++ src/BRDFReader/BRDFReader.hpp
@@ -15,6 +15,7 @@ namespace ChefDevr {
         const auto num_brdfs = brdf_filePaths.size();
         Matrix<Scalar> Z{num_brdfs, num_coefficientsBRDF};
 
+#pragma omp parallel for
         for (unsigned int i = 0; i < num_brdfs; ++i) {
             Z.row(i) = read_brdf<Scalar>(i);
         }
@@ -36,6 +37,7 @@ namespace ChefDevr {
             ZZt_centered(i, i) = brdf_first.dot(brdf_first);
             meanBRDF += brdf_first;
 
+#pragma omp parallel for
             for (unsigned int j = i + 1; j < num_brdfs; ++j) {
                 const RowVector<Scalar> brdf_second = read_brdf<Scalar>(j);
                 const Scalar coefficient = brdf_first.dot(brdf_second);
@@ -46,6 +48,8 @@ namespace ChefDevr {
         meanBRDF /= num_brdfs;
 
         RowVector<Scalar> brdf_brdfMean{num_brdfs};
+
+#pragma omp parallel for
         for (unsigned int i = 0; i < num_brdfs; ++i) {
             const RowVector<Scalar> brdf = read_brdf<Scalar>(i);
             brdf_brdfMean(i) = brdf.dot(meanBRDF);
